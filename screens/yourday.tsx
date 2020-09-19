dee2434f0cad6bdb69a390b61a6a9a6c90d20620
jiaju1103 <jiaju@mit.edu>
2020-09-19 15:50:21 -0500

    test2

--- screens/yourday.tsx
+++ screens/yourday.tsx
@@ -7,6 +7,7 @@ export default function YourDay() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Your Day</Text>
+      <Text style={styles.title}>Graph1</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
