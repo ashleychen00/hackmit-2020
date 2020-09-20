fde1eb811b57b6058449a47238573047651aa57c
ashley22 <ashley22@mit.edu>
2020-09-19 20:34:59 -0400

    followed native-base setup instructions

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { AppLoading } from 'expo';
+import { Container, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
-    return null;
+    return <AppLoading />;
   } else {
     return (
       <SafeAreaProvider>
