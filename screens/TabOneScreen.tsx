9d1687edf3b1d76112aa63da0b55a1ab17c0d7a8
jiaju1103 <jiaju@mit.edu>
2020-09-19 21:13:16 -0500

    added progress circle, disregard tab1 + 2

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,34 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {Picker, Text, StyleSheet, View, TextInput, Button} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-export default function TabOneScreen() {
+const App = () => {
+  const [currency, setCurrency] = useState('US Dollar');
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View >
+      <Text > Demo Form </Text>
+      <View>
+        <TextInput 
+          placeholder="Email" />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+        />
+        <Picker
+          selectedValue={currency}
+          onValueChange={currentCurrency => setCurrency(currentCurrency)}>
+          <Picker.Item label="USD" value="US Dollars" />
+          <Picker.Item label="EUR" value="Euro" />
+          <Picker.Item label="NGN" value="Naira" />
+        </Picker>
+        <Text>
+          Selected: {currency}
+        </Text>
+      </View>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  //Check project repo for styles
 });
+
+export default App;
